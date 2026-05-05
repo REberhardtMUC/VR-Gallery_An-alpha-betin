1013a5440fb18b77d8384be0cd030aabcc81359b
Rosalie Eberhardt aka Rosalie Rousseau <info@RosalieEberhardt.de>
2026-05-05 11:47:42 +0200

    Shade diskreter
    sw und semi transparent

---
+++ Assets/Gallery Assets/Scripts/SetNextPath.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetNextPath : MonoBehaviour
+{
+
+    public GameObject nextPath;
+    // Start is called before the first frame update
+
+    private void OnTriggerEnter(Collider other)
+    {
+        nextPath.SetActive(true);
+        //nextPath.SetActive(false);
+        // TO DO Alle anderen Pfade ausblenden
+    }
+}
